8e9367c6f873bb4785c202ee8535d4093de5cea9
William Donovan-Seid <wdstwo@live.com>
2020-11-20 08:46:00 -0800

    add initial template

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <StoreControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
